8c0ddd482a0cf11dc052b0c83dae8423af397683
germanale <101187591+germanale@users.noreply.github.com>
2022-04-21 08:46:37 -0400

    Add files via upload

---
+++ CODING DOJO.js
@@ -0,0 +1,12 @@
+
+var alturaNiÃ±o = 45;
+function muestraSiElNiÃ±oPuedeSubiraLaMontaÃ±aRusa () {
+    if (alturaNiÃ±o >= 52) {
+        console.log ("Â¡SÃºbete, Chico!")
+    }
+    else {
+        alturaNiÃ±o < 52; {
+            console.log ("Lo siento, Chico. Tal vez el prÃ³ximo aÃ±o")
+        }
+    }
+}
